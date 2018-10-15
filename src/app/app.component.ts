dff3bbf2a722480a3cecb339a01160a3773739b1
Zita Horváth <zita.horvath@sigmatechnology.se>
2018-10-15 21:51:53 +0200

    Create main dashboard and add routing for storelist and itemlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Shopping List';
+
+  constructor(
+  ) {}
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StoreListComponent } from './store-list/store-list.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'store-list', component: StoreListComponent },
+  { path: 'item-list', component: ItemListComponent },
+  { path: '', component: MainDashboardComponent },
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoreListComponent,
+    ItemListComponent,
+    MainDashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only      
+    ),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
